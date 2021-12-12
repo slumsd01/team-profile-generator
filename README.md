cc4729624d06a4e7ec5de528952ba99a341e02eb
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2021-12-11 22:26:45 -0500

    requirements met

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# team-profile-generator
+# Team Profile Generator
+
+## Description
+
+This command-line application can be used to create teams in the work place to better divide a project's workload.
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+This application was built using basic HTML, CSS, and Javascript. But it also requires npms such as [Bootstrap](https://www.npmjs.com/package/bootstrap) for visual styling, [Inquirer](https://www.npmjs.com/package/inquirer) for basic functionality, and [Jest](https://www.npmjs.com/package/jest) for testing.
+
+## Usage
+
+
+
+## Tests
+
+Tests are provided, and they can be run using Jest npm.
--- index.js
+++ index.js
@@ -1,234 +1,106 @@
 const inquirer = require('inquirer')
-const fs = require('fs');
 
 const Employee = require('./lib/Employee')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 const Manager = require('./lib/Manager')
 
+const { generateHTML , generateCSS } = require('./lib/generatePages')
 
+let employee
 let employeeArray = []
 
-const questions = [
+const managerQuestions = [
     {
         type: 'input',
         name: 'name',
-        message: "What is the employee's name?",
+        message: "What is the team manager's name?",
     },
     {
         type: 'input',
         name: 'id',
-        message: "What is the employee's ID number?",
+        message: "What is this manager's ID number?",
     },
     {
         type: 'input',
         name: 'email',
-        message: "What is this employee's email?",
+        message: "What is this manager's email?",
     },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is this manager's office number?",
+    },
+]
+
+const questions = [
     {
         type: 'list',
         name: 'role',
-        message: "What is this employee's role?",
-        choices: ['Engineer', 'Intern', 'Manager']
+        message: "What is the this team member's role?",
+        choices: ['Engineer', 'Intern']
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is this team member's name?",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is this team member's ID number?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is this team member's email?",
     },
     {
         type: 'input',
         name: 'github',
-        message: "What is this employee's GitHub username?",
+        message: "What is this team member's GitHub username?",
         when: (answers) => answers.role == 'Engineer',
     },
     {
         type: 'input',
         name: 'school',
-        message: "What school does this employee attend?",
+        message: "What school does this team member attend?",
         when: (answers) => answers.role == 'Intern',
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: "What is this employee's office number?",
-        when: (answers) => answers.role == 'Manager',
     },
 ]
 
+function inquireManager() {
+    inquirer
+    .prompt(managerQuestions)
+    .then((managerAnswers) => {
+        employee = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, 'Manager', managerAnswers.officeNumber)
 
-function generateHTML() {
-    //start HTML
-    let HTML = `
-    <!doctype html>
-    <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
+        employeeArray.push(employee)
 
-        <title>My Team</title>
-    </head>
-    <body>
-        <header>
-            <h1>My Team</h1>
-        </header>
-        <section class="row justify-content-center" id="container">
-    `
-
-    let HTMLend = `
-        </section>
-    </body>
-    </html>
-    `
-
-    //generate manager card
-    employeeArray.forEach(Manager => {
-        if (Manager.role == 'Manager') {
-            HTML += `
-            <div class="card" style="width: 18rem;">
-                <div class="card-header">
-                    <h5 class="card-title" id="name">
-                        ${Manager.name}
-                    </h5>
-                    <h6 class="card-subtitle mb-2">
-                        ${Manager.role}
-                    </h6>
-                </div>
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item">
-                    ID: ${Manager.id}
-                </li>
-                <li class="list-group-item">
-                    Email: <a href="mailto:${Manager.email}"> ${Manager.email} </a>
-                </li>
-                <li class="list-group-item">
-                    Office Number: ${Manager.officeNumber}
-                </li>
-                </ul>
-            </div>
-            `
-        }
-    })    
-    
-    //generate engineer cards
-    employeeArray.forEach(Engineer => {
-        if (Engineer.role == 'Engineer') {
-            HTML += `
-            <div class="card" style="width: 18rem;">
-                <div class="card-header">
-                    <h5 class="card-title" id="name">
-                        ${Engineer.name}
-                    </h5>
-                    <h6 class="card-subtitle mb-2">
-                        ${Engineer.role}
-                    </h6>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">
-                      ID: ${Engineer.id}
-                  </li>
-                  <li class="list-group-item">
-                      Email: <a href="mailto:${Engineer.email}"> ${Engineer.email} </a>
-                  </li>
-                  <li class="list-group-item">
-                      GitHub: <a href="https://github.com/${Engineer.github}" target="blank"> ${Engineer.github} </a>
-                  </li>
-                </ul>
-            </div>
-            `
-        }
-    })
-
-    //generate intern cards
-    employeeArray.forEach(Intern => {
-        if (Intern.role == 'Intern') {
-            HTML += `
-            <div class="card" style="width: 18rem;">
-                <div class="card-header">
-                    <h5 class="card-title" id="name">
-                        ${Intern.name}
-                    </h5>
-                    <h6 class="card-subtitle mb-2">
-                        ${Intern.role}
-                    </h6>
-                </div>
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item">
-                    ID: ${Intern.id}
-                </li>
-                <li class="list-group-item">
-                    Email: <a href="mailto:${Intern.email}"> ${Intern.email} </a>
-                </li>
-                <li class="list-group-item">
-                    School: ${Intern.school}
-                </li>
-                </ul>
-            </div>
-            `
-        }
-    })
-
-    //finish HTML
-    HTML += HTMLend
-
-    fs.writeFile('./dist/index.html', HTML, function (err) {
-        if (err) {
-            console.log("Error creating HTML file.")
-        } else {
-            console.log("HTML file created.")
-        }
+        inquirer
+        .prompt({
+            type: 'confirm',
+            name: 'buildTeam',
+            message: "Manager added. Would you like to build a team?",
+        })
+        .then((confirm) => {
+            if (confirm.buildTeam == true) {
+                inquireEmployees();
+            }
+        })
     })
 }
 
-function generateCSS() {
-    let cssTemplate = `
-    body {
-        margin: 30px;
-    }
 
-    header {
-        text-align: center;
-        background-color: #E84756;
-        color: white;
-        padding: 30px;
-        margin: 20px 0;
-    }
-
-    .card {
-        margin: 10px;
-    }
-
-    .card-header {
-        background-color: #0077F7;
-        color: white;
-        margin: 0 -12px;
-    }
-
-    .card-subtitle {
-        opacity: 0.8;
-    }
-    `
-
-    fs.writeFile('./dist/style.css', cssTemplate, function (err) {
-        if (err) {
-            console.log("Error creating CSS file.")
-        } else {
-            console.log("CSS file created.")
-        }
-    })
-}
-
-function initialize(){
+function inquireEmployees(){
     inquirer
     .prompt(questions)
     .then((answers) => {
-        let employee;
-
         if (answers.role == 'Engineer') {
             employee = new Engineer(answers.name, answers.id, answers.email, answers.role, answers.github)
         }
         if (answers.role == 'Intern') {
             employee = new Intern(answers.name, answers.id, answers.email, answers.role, answers.school)
-        }
-        if (answers.role == 'Manager') {
-            employee = new Manager(answers.name, answers.id, answers.email, answers.role, answers.officeNumber)
         }
 
         employeeArray.push(employee)
@@ -236,17 +108,20 @@ function initialize(){
         inquirer.prompt({
             type: 'confirm',
             name: 'nextEmployee',
-            message: "New employee added! Would you like to add another?",
+            message: "New team member added! Would you like to add another?",
         })
         .then((confirm) => {
             if (confirm.nextEmployee == true) {
-                initialize();
+                inquireEmployees();
             } else {
-                generateHTML();
+                module.exports = employeeArray
+                generateHTML(employeeArray);
                 generateCSS();
+
+                console.log(`Your files have been generated! They can be found in the 'dist/' folder.`)
             }
         })
     })
 }
 
-initialize();
+inquireManager();
